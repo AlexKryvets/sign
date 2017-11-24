eaebf88d620c889867571051fd6ef5e84cba6c36
Alexey Kryvets <alex.kryvets@gmail.com>
2017-11-24 18:05:54 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ENV = process.env.npm_lifecycle_event;
+const isDev = ENV === 'build:dev';
+const isProd = ENV === 'build:prod';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+var config = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'sign.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.html']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts-loader'],
+                include: [/node_modules/, path.resolve(__dirname, 'src')]
+            }
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            notify: false,
+            open: false,
+            host: 'localhost',
+            port: 3000,
+            tunnel: true,
+            server: {
+                baseDir: './'
+            }
+        })
+    ]
+};
+
+if (isProd) {
+    config.plugins.push(
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.UglifyJsPlugin({mangle: {keep_fnames: true}})
+    );
+}
+
+module.exports = config;
